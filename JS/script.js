7deddc04a658721bf4cf7e390121efeafde9cc57
Aman1869 <sinhajack619@gmail.com>
2024-08-24 12:08:17 +0530

    New Changes made

--- JS/script.js
+++ JS/script.js
@@ -31,6 +31,7 @@ async function getsongs(folder) {
             songs.push(element.href.split(`/${folder}/`)[1])
         }
     }
+    
     //Show all the songs in the playlist.
     let songul = document.querySelector(".songlist").getElementsByTagName("ul")[0];
     songul.innerHTML = ""
@@ -75,7 +76,7 @@ async function displayAlbums() {
     let array = Array.from(anchors)
     for (let index = 0; index < array.length; index++) {
         const e = array[index]
-        if (e.href.includes("/songs/") && !e.href.includes(".htaccess")) {
+        if (e.href.includes("/songs/")) {
             let folder = e.href.split("/").slice(-1)[0];
             // //Get the metadata of the folder
             let a = await fetch(`/songs/${folder}/info.json`);
